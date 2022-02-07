af6006a86194fcbe2b690b2516a2d16ece63b50d
Adam Tharani <adamtharani@me.com>
2022-02-07 18:30:01 -0500

    Inital Commit
    Signed-off-by: Adam Tharani <adamtharani@me.com>

---
+++ tutorial_3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  tutorial_3
+//
+//  Created by Adam Tharani on 2022-02-07.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
