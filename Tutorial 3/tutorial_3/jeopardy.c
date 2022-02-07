083feac7504afa32d5381a4e952bd7c3823e2623
Adam Tharani <30530469+adamtharani@users.noreply.github.com>
2022-02-07 18:23:17 -0500

    Tutorial 3
    Inital Commit
    
    Signed-off-by: Adam Tharani <adamtharani@me.com>
    
    wip
    
    Signed-off-by: Adam Tharani <adamtharani@me.com>
    
    Added questions and answers for the categories. Display the game board.
    
    added display question, update_score, and player_exists
    
    Update temp.c
    
    added already_answered
    
    valid answer
    
    Signed-off-by: Adam Tharani <adamtharani@me.com>
    
    Cleeanup
    
    Signed-off-by: Adam Tharani <adamtharani@me.com>
    
    it works
    
    Signed-off-by: Adam Tharani <adamtharani@me.com>
    
    Final
    
    x
    
    Signed-off-by: Adam Tharani <adamtharani@me.com>
    
    Folder
    
    Signed-off-by: Adam Tharani <adamtharani@me.com>

---
+++ Tutorial 3/tutorial_3/jeopardy.c
@@ -0,0 +1,119 @@
+/*
+ * Tutorial 3 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "questions.h"
+#include "players.h"
+#include "jeopardy.h"
+
+// Put macros or constants here using #define
+#define BUFFER_LEN 256
+#define NUM_PLAYERS 2
+
+// Put global environment variables here
+
+// Processes the answer from the user containing what is or who is and tokenizes it to retrieve the answer.
+void tokenize(char *input, char **tokens) {
+    char *token;
+
+    token = strtok(input, " ");
+
+    if(token != NULL){
+        if(strcasecmp(token, "who") != 0 && strcasecmp(token, "what") != 0){
+            return;
+        }
+    }
+
+    token = strtok(NULL, " ");
+
+    if(token != NULL){
+        if(strcasecmp(token, "is") != 0){
+            return;
+        }
+    }
+
+    *tokens = strtok(NULL, "\n");
+}
+
+
+// Displays the game results for each player, their name and final score, ranked from first to last place
+void show_results(player *players) {
+    for(int i = 1; i <= NUM_PLAYERS; i++){
+        printf("Name: %s    Score: %d\n", players[i].name, players[i].score);
+    }
+}
+
+
+int main() //(int argc, char *argv[])
+{
+    // An array of 4 players, may need to be a pointer if you want it set dynamically
+    player players[NUM_PLAYERS];
+    
+    // Input buffer and and commands
+    char buffer[BUFFER_LEN] = { 0 };
+
+    // Display the game introduction and initialize the questions
+    printf("\nWelcome to Jeopardy! (Maximum of 4 players)\n\n");
+    initialize_game();
+
+    for (int count = 1; count <= NUM_PLAYERS; count++) {
+
+        char name[256];
+        printf("Please enter the name for player %i:\n", count);
+        scanf("%s", name);
+        player p;
+        strcpy(p.name, name);
+        p.score = 0;
+        players[count] = p;
+        printf("Successfully stored %s as player %i\n\n", name, count);
+   }
+    int playerCounter = 1;
+    int questions_remaining = sizeof(questions);
+//     Perform an infinite loop getting command input from users until game ends
+    while (fgets(buffer, BUFFER_LEN, stdin) != NULL) {
+        while (questions_remaining >= 0)
+        {
+
+            player currentPlayer = players[playerCounter];
+            printf("The current player is %s\n\n", currentPlayer.name);
+            char categorySelection[BUFFER_LEN];
+            int valueSelection;
+            char answer[BUFFER_LEN];
+            display_categories();
+            printf("Please pick a category: \n");
+            scanf("%s", categorySelection);
+            printf("Please pick a value: \n");
+            scanf("%i", &valueSelection);
+            display_question(categorySelection, valueSelection);
+            fflush(stdin);
+            printf("Please enter your answer: \n");
+            scanf("%[^\n]s", answer);
+            char *tokenizedAnswer;
+            tokenize(answer, &tokenizedAnswer);
+            if (valid_answer(categorySelection, valueSelection, tokenizedAnswer)) {
+                printf("\nYou answered right\n");
+                update_score(players, NUM_PLAYERS, currentPlayer.name, currentPlayer.score + valueSelection);
+                show_results(players);
+            }
+            else {
+                playerCounter++;
+                display_answer(categorySelection, valueSelection);
+            }
+
+            mark_question_answered(categorySelection, valueSelection);
+            questions_remaining--;
+
+        }
+    }
+        return EXIT_SUCCESS;
+}
+
+
+
---
+++ Tutorial 3/tutorial_3/questions.c
@@ -0,0 +1,202 @@
+/*
+ * Tutorial 3 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
+ *
+ * Copyright (C) 2015, <GROUP MEMBERS>
+ * All rights reserved.
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "questions.h"
+
+// Initializes the array of questions for the game
+void initialize_game(void)
+{
+    // initialize each question struct and assign it to the questions array
+    
+    // "programming", 
+    //"algorithms", 
+    //"databases"
+    question q1;
+    q1.value = 100;
+    strcpy(q1.category, categories[0]);
+    strcpy(q1.question, "This is used to write instructions for the computer. A computer executes a sequence of instructions (a program) in order to perform some task.");
+    strcpy(q1.answer, "programming");
+    q1.answered = false;
+    questions[0] = q1;
+
+    question q2;
+    q2.value = 200;
+    strcpy(q2.category, categories[0]);
+    strcpy(q2.question, "The data that you put into the computer system for processing.");
+    strcpy(q2.answer, "input");
+    q2.answered = false;
+    questions[1] = q2;
+
+    question q3;
+    q3.value = 300;
+    strcpy(q3.category, categories[0]);
+    strcpy(q3.question, "Any data that can be true/false");
+    strcpy(q3.answer, "boolean");
+    q3.answered = false;
+    questions[2] = q3;
+
+    question q4;
+    q4.value = 400;
+    strcpy(q4.category, categories[0]);
+    strcpy(q4.question, "a container for storing values, information or data in general. The information inside them can be used and changed.");
+    strcpy(q4.answer, "variable");
+    q4.answered = false;
+    questions[3] = q4;
+
+    question q5;
+    q5.value = 100;
+    strcpy(q5.category, categories[1]);
+    strcpy(q5.question, "A well defined procedure solve a problem");
+    strcpy(q5.answer, "algorithm");
+    q5.answered = false;
+    questions[4] = q5; 
+
+    question q6;
+    q6.value = 200;
+    strcpy(q6.category, categories[1]);
+    strcpy(q6.question, "a sequence that gets executed multiple times");
+    strcpy(q6.answer, "loop");
+    q6.answered = false;
+    questions[5] = q6; 
+
+    question q7;
+    q7.value = 300;
+    strcpy(q7.category, categories[1]);
+    strcpy(q7.question, "A body of code that has a specific functionality");
+    strcpy(q7.answer, "method");
+    q7.answered = false;
+    questions[6] = q7; 
+
+    question q8;
+    q8.value = 400;
+    strcpy(q8.category, categories[1]);
+    strcpy(q8.question, "an algorithm that repeatedly swapping the previous elements if they are in the wrong order");
+    strcpy(q8.answer, "bubble");
+    q8.answered = false;
+    questions[7] = q8; 
+
+    question q9;
+    q9.value = 100;
+    strcpy(q9.category, categories[2]);
+    strcpy(q9.question, "Each row in access");
+    strcpy(q9.answer, "record");
+    q9.answered = false;
+    questions[8] = q9; 
+
+    question q10;
+    q10.value = 200;
+    strcpy(q10.category, categories[2]);
+    strcpy(q10.question, "an organized collection of information");
+    strcpy(q10.answer, "database");
+    q10.answered = false;
+    questions[9] = q10; 
+
+    question q11;
+    q11.value = 300;
+    strcpy(q11.category, categories[2]);
+    strcpy(q11.question, "Link data between tables for queries");
+    strcpy(q11.answer, "relationship");
+    q11.answered = false;
+    questions[10] = q11; 
+
+    question q12;
+    q12.value = 400;
+    strcpy(q12.category, categories[2]);
+    strcpy(q12.question, "A different parts of each record");
+    strcpy(q12.answer, "field");
+    q12.answered = false;
+    questions[11] = q12; 
+}
+
+// Displays each of the remaining categories and question dollar values that have not been answered
+void display_categories(void)
+{
+    char tempQuestions[NUM_QUESTIONS][20];
+    
+    for (int i = 0; i < NUM_QUESTIONS; i++){
+        if (questions[i].answered == false){
+            snprintf(tempQuestions[i], 20, "%d", questions[i].value);
+        }
+
+        else {
+            strcpy(tempQuestions[i], "---");
+        }
+    }
+    // print categories and dollar values for each unanswered question in questions array
+
+    printf("    %s  |   %s   |   %s  \n", categories[0], categories[1], categories[2]);
+    printf("--------------------------------------------------\n");
+    for (int i=0; i<4; i++){
+            printf("       %s       |       %s      |      %s             \n", tempQuestions[i], tempQuestions[i+4], tempQuestions[i+8]);
+            printf("--------------------------------------------------\n");
+    }
+    printf("\n");
+}
+
+// Displays the question for the category and dollar value
+void display_question(char *category, int value)
+{
+    printf("\nYou choose %s for $%d \n", category, value);
+    
+    for (int i = 0; i < NUM_QUESTIONS; i++){
+        //If the value of category and value match return the question number
+        if (strcmp (questions[i].category, category) == 0 && (questions[i].value == value)){
+            printf("Question: %s\n\n", questions[i].question);
+        }
+    }
+}
+
+// Returns true if the answer is correct for the question for that category and dollar value
+bool valid_answer(char *category, int value, char *answer)
+{
+    for (int i = 0; i < NUM_QUESTIONS; i++) {
+        if (strcmp(questions[i].category, category) == 0) {
+            if (questions[i].value == value)
+                if (strcmp(questions[i].answer, answer) == 0) {
+                    return true;
+                }
+        }
+    }
+	return false;
+}
+
+// Returns true if the question has already been answered
+bool already_answered(char *category, int value)
+{
+    //Go through each category
+    for (int i = 0; i < NUM_QUESTIONS; i++){
+        //If the category and value matches, return true
+        if (strcmp(questions[i].category, category) == 0 && (questions[i].value = value) && (questions[i].answered == true) ){
+            return true;
+        }
+    }
+    return false;
+}
+
+void mark_question_answered(char *category, int value) {
+    for (int i = 0; i < NUM_QUESTIONS; i++) {
+        if (strcmp(questions[i].category, category) == 0) {
+            if (questions[i].value == value) {
+                questions[i].answered = true;
+            }
+        }
+    }
+}
+
+void display_answer(char *category, int value)
+{
+    for (int i = 0; i <= NUM_QUESTIONS; i++){
+        //If the value of category and value match return the question number
+        if (strcmp (questions[i].category, category) == 0 && (questions[i].value == value)){
+            printf("\nYou answered wrong! The correct answer is: %s\n", questions[i].answer);
+            return;
+        }
+    }
+}
