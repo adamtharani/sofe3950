faac5d77aee26f547c24c6af2a8f9a3b3915f576
Adam Tharani <adamtharani@me.com>
2022-03-25 21:56:51 -0400

    Tutorial 5 wip
    Signed-off-by: Adam Tharani <adamtharani@me.com>

---
+++ tutorial_5/tutorial_5/tutorial_5/main.c
@@ -0,0 +1,132 @@
+//
+//  main.c
+//  tutorial_5
+//
+//  Created by Adam Tharani on 2022-03-25.
+//
+#define _XOPEN_SOURCE 700
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+void question1(void);
+void question2(void);
+void question3(void);
+void writeFile(char *body, char *fileName);
+void readFile(char *fileName);
+struct factorialInput {
+    int number;
+    int index ;
+};
+int factorial_Custom(struct factorialInput input);
+
+
+int main(int argc, const char * argv[]) {
+    question3();
+    return 0;
+}
+
+void question1(void) {
+    pid_t child_a, child_b;
+    
+    child_a = fork();
+    
+    if (child_a == 0) {
+        /* Child A code */
+        sleep(1);
+        printf("I am child 1 \n");
+        readFile("child1.txt");
+        
+    } else {
+        child_b = fork();
+        
+        if (child_b == 0) {
+            /* Child B code */
+            sleep(1);
+            printf("I am child 2 \n");
+            readFile("child2.txt");
+        } else {
+            /* Parent Code */
+            writeFile("child 1", "child1.txt");
+            writeFile("child 2", "child2.txt");
+        }
+    }
+}
+
+void question2(void) {
+    pid_t child_a;
+    int child_a_status;
+    
+    child_a = fork();
+    
+    if (child_a == 0) {
+        /* Child A code */
+        sleep(1);
+        printf("Child process\n");
+    } else {
+        /* Parent Code */
+        wait(&child_a_status);
+        printf("Parent process\n");
+        printf("Exit status: %d\n", WEXITSTATUS(child_a_status));
+    }
+}
+
+int moving_sum[5] = {0, 0, 0, 0, 0};
+
+void question3(void) {
+    for (int counter = 0; counter < 5; counter++) {
+        printf("Please enter a number: ");
+        scanf("%d", &moving_sum[counter]);
+        printf("You entered %d\n", moving_sum[counter]);
+        
+        struct factorialInput input;
+        input.index = counter;
+        input.number = moving_sum[counter];
+        
+        printf("%d\n", factorial_Custom(input));
+    }
+    
+}
+
+void writeFile(char *body, char *fileName) {
+    FILE *fp;
+    
+    // Open file to write the text in
+    fp = fopen(fileName, "w");
+    if (fp == NULL) {
+        printf("Cannot open file \n");
+        exit(0);
+    }
+    fprintf(fp, "%s", body);
+    
+    //Close file
+    fclose(fp);
+}
+
+void readFile(char *fileName) {
+    FILE *fp;
+    struct stat sb;
+    stat(fileName, &sb);
+    char *file_contents = malloc(sb.st_size);
+    
+    // Open file to write the text in
+    fp = fopen(fileName, "r");
+    if (fp == NULL) {
+        printf("Cannot open file \n");
+        exit(0);
+    }
+    while (fscanf(fp, "%[^\n] ", file_contents) != EOF) {
+        printf("Value of file %s is %s\n", fileName, file_contents);
+    }
+}
+
+int factorial_Custom(struct factorialInput input) {
+    int factorial_value = 1;
+    for (int counter = 1; counter <= input.number; counter++) {
+        factorial_value *= counter;
+    }
+    return factorial_value;
+}
